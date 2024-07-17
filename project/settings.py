a815407bef30a431764e31432ef0f2bc7d970455
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-07-17 06:10:18 +0300

    корсы

--- project/settings.py
+++ project/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 LOCAL_WORKING = False
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://subway-ekzament-backend.onrender.com',
+    '*'
+]
 CSRF_TRUSTED_ORIGINS = [
     'https://subway-ekzament-backend.onrender.com',
 ]
